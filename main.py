4faab853ea409bbdb6b48a4f22ca7f5768c0c802
Aditya Desai <desaiaditya72@gmail.com>
2024-09-03 12:07:07 +0530

    4th commit

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import PlainTextResponse, JSONResponse
+from fastapi.responses import JSONResponse
 import uvicorn
 import firebase_admin
 from firebase_admin import credentials, firestore
 from firebase_admin.firestore import firestore as fs
 import os
-import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,15 +12,31 @@ load_dotenv()
 app = FastAPI()
 
 # Load the service account key from the environment variable
-service_account_info = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+private_key=os.getenv('private_key')
+private_key_id=os.getenv('private_key_id')
+client_email=os.getenv('client_email')
+project_id=os.getenv('project_id')
+client_id=os.getenv('client_id')
+client_x509_cert_url=os.getenv('client_x509_cert_url')
 
-if service_account_info:
-    cred = credentials.Certificate(json.loads(service_account_info))
-    firebase_admin.initialize_app(cred)
-else:
-    raise ValueError("No service account info found in environment variable.")
+cred_dict = {
+  "type": "service_account",
+  "project_id": project_id,
+  "private_key_id": private_key_id,
+  "private_key": private_key,
+  "client_email": client_email,
+  "client_id": client_id,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": client_x509_cert_url,
+  "universe_domain": "googleapis.com"
+}
 
+cred = credentials.Certificate(cred_dict)
+firebase_admin.initialize_app(cred)
 db = firestore.client()
+
 
 @app.post("/")
 async def receive_webhook(request: Request):
